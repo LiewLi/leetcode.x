7e7436106594d69ef636616a59816c99a813f5b2
LiewLi <liulimg@gmail.com>
2019-09-08 01:34:04 +0800

    sync

---
+++ 738.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var monotoneIncreasingDigits = function(N) {
+    let s = "" + N
+    let sArr = s.split('')
+    let idx = sArr.length
+    for (let i = sArr.length-1; i > 0; --i) {
+        if (sArr[i] < sArr[i-1]) {
+            idx = i
+            sArr[i-1] = '' + ((+sArr[i-1]) - 1)
+        }
+    }
+    let ret = sArr.slice(0, idx).join('')
+    for (let i = idx; i < s.length; ++i) {
+        ret += '9'
+    }
+    return +ret
+};
+
+console.log(monotoneIncreasingDigits(332))
