019be91ccf2e84feebec0ced08bbb8c452bf33da
LiewLi <liulimg@gmail.com>
2019-09-13 10:25:03 +0800

    sync

---
+++ 313.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number[]} primes
+ * @return {number}
+ */
+var nthSuperUglyNumber = function(n, primes) {
+    primes.sort()
+    let ugly = new Array(n).fill(Number.MAX_SAFE_INTEGER)
+    let idx = new Array(primes.length).fill(0)
+    ugly[0] = 1
+    for (let i = 1; i < n; ++i) {
+        for (let j = 0; j < primes.length; ++j) {
+            ugly[i] = Math.min(ugly[i], ugly[idx[j]] * primes[j])
+        }
+        for (let j = 0; j < primes.length; ++j) {
+            idx[j] += (primes[j] * ugly[idx[j]]) == ugly[i] ? 1 : 0
+        }
+    }
+
+    return ugly[n-1]
+};
+
+console.log(nthSuperUglyNumber(2, [2, 3, 5]))
