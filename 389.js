94609fd3984fbecfec6e5d3452cc1e389c59487f
LiewLi <liulimg@gmail.com>
2019-09-15 16:11:06 +0800

    sync

--- 389.js
+++ 389.js
@@ -4,8 +4,8 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let a = s.split('').sort()
-    let b = t.split('').sort()
+    const a = s.split('').sort()
+    const b = t.split('').sort()
     for (let i = 0; i < a.length; ++i) {
         if (b[i] !== a[i]) return b[i]
     }
