8b5d745e1357596f384f7dd3e1ebd0d565174134
LiewLi <liulimg@gmail.com>
2019-09-18 00:38:35 +0800

    sync

---
+++ 942.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @return {number[]}
+ */
+var diStringMatch = function(S) {
+    const n = S.length
+    let low = 0
+    let high = n
+    const ret = []
+    for (let i = 0; i < S.length; ++i) {
+        if (S[i] === 'I') {
+            ret.push(low++)
+        } else {
+            ret.push(high--)
+        }
+    }
+    ret.push(low)
+    return ret
+};
+
+console.log(diStringMatch('IDID'))
