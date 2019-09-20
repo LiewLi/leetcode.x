14c98f5facac79f17e82cc74966f70d8f8dfef76
LiewLi <liulimg@gmail.com>
2019-09-21 01:56:10 +0800

    sync

---
+++ 599.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    const map = {}
+    list1.forEach((e, idx) => {
+        map[e] = idx
+    })
+
+    let min = Infinity
+    let ret = []
+    list2.forEach((e, idx) => {
+        if (e in map) {
+            const sum = map[e] + idx
+            if (sum < min) {
+                min = sum
+                ret = [e]
+            } else if (sum == min) {
+                ret.push(e)
+            }
+        }
+    })
+
+    return ret
+};
