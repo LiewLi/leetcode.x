615c41c07f4f6645682cae7f1ffb3093cb8564bc
LiewLi <liulimg@gmail.com>
2019-09-23 22:24:28 +0800

    sync

---
+++ 648.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} dict
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function(dict, sentence) {
+    dict.sort((a, b) => a.length - b.length)
+    return sentence.replace(/(\w*)/g, (m, word) => {
+        for (const r of dict) {
+            if (word.indexOf(r) === 0) {
+                return r
+            }
+        }
+        return word
+    })
+};
