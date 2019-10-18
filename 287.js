104e8dd330baf4e8321aefed62e7e1e477fb13d6
LiewLi <liulimg@gmail.com>
2019-10-19 01:39:16 +0800

    sync

---
+++ 287.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(nums) {
+  let slow = nums[0];
+  let fast = nums[nums[0]];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  }
+
+  fast = 0;
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return fast;
+};
