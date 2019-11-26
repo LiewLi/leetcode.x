04fa9262bf7754409e808997e4fc37a518fb4973
LiewLi <liulimg@gmail.com>
2019-11-27 01:32:58 +0800

    sync

---
+++ 376.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var wiggleMaxLength = function(nums) {
+  if (nums.length <= 0) return 0;
+  let down = 1;
+  let up = 1;
+  for (let i = 1; i < nums.length; ++i) {
+    if (nums[i] > nums[i - 1]) {
+      up = down + 1;
+    } else if (nums[i] < nums[i - 1]) {
+      down = up + 1;
+    }
+  }
+  return Math.max(down, up);
+};
