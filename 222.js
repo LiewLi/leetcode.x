0a7962206483828519f9178c3975e11f00f928ea
L1 <liulimg@gmail.com>
2020-01-28 14:17:21 +0800

    sync

---
+++ 222.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+  if (!root) return 0;
+  let left = root,
+    right = root;
+  let lh = 0,
+    rh = 0;
+
+  while (left) {
+    left = left.left;
+    lh++;
+  }
+
+  while (right) {
+    right = right.right;
+    rh++;
+  }
+
+  if (lh === rh) return (1 << lh) - 1;
+  else return 1 + countNodes(root.left) + countNodes(root.right);
+};
