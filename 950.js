15e8beac808ab5c8b9417fd5fc113a764bcb6d8e
LiewLi <liulimg@gmail.com>
2020-02-29 23:56:11 +0800

    sync

---
+++ 950.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} deck
+ * @return {number[]}
+ */
+var deckRevealedIncreasing = function(deck) {
+  const N = deck.length;
+  const ret = [];
+  const idx = [];
+  for (let i = 0; i < N; ++i) {
+    idx[i] = i;
+  }
+  deck.sort((a, b) => a - b);
+  for (const d of deck) {
+    ret[idx.shift()] = d;
+    if (ret.length) idx.push(idx.shift());
+  }
+  return ret;
+};
+
+console.log(deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7]));
