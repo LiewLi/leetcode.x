1e3ae171b30d24227ffc70129f2903819a604c09
LiewLi <liulimg@gmail.com>
2020-03-24 23:11:58 +0800

    sync

--- 1014.js
+++ 1014.js
@@ -3,20 +3,18 @@
  * @return {number}
  */
 var maxScoreSightseeingPair = function(A) {
-  const arr1 = [];
-  const arr2 = [];
+  const arr = [];
   A.forEach((a, idx) => {
-    arr1[idx] = a - idx;
-    arr2[idx] = a + idx;
+    arr[idx] = a + idx;
   });
-  for (let i = 1; i < arr2.length; ++i) {
-    arr2[i] = Math.max(arr2[i], arr2[i - 1]);
+  for (let i = 1; i < arr.length; ++i) {
+    arr[i] = Math.max(arr[i], arr[i - 1]);
   }
 
   let max = 0;
 
-  for (let i = 1; i < arr1.length; ++i) {
-    max = Math.max(max, arr1[i] + arr2[i - 1]);
+  for (let i = 1; i < A.length; ++i) {
+    max = Math.max(max, A[i] - i + arr[i - 1]);
   }
 
   return max;
