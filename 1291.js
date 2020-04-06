d0cb58f7ce6176b2bd679af75482b3f461b82572
LiewLi <liulimg@gmail.com>
2020-04-06 13:35:02 +0800

    sync

---
+++ 1291.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+  let d = 1;
+  let len = 0;
+  let n = low;
+  while (n) {
+    len++;
+    n = Math.floor(n / 10);
+  }
+
+  const ret = [];
+
+  const _ = (n, d) => {
+    let v = 0;
+    for (let i = n; i < d + n; ++i) {
+      v = 10 * v + i;
+    }
+    return v;
+  };
+
+  let v = _(d, len);
+  while (v <= high) {
+    if (v >= low) {
+      ret.push(v);
+    }
+    d++;
+    if (d + len > 10) {
+      len++;
+      d = 1;
+    }
+    v = _(d, len);
+  }
+  return ret;
+};
+
+console.log(sequentialDigits(1000, 13000));
