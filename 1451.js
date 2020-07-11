f61094de44e0c9cf82f904050cf195bdf2060aad
LiewLi <liulimg@gmail.com>
2020-07-11 13:23:24 +0800

    sync

--- 1451.js
+++ 1451.js
@@ -5,10 +5,7 @@
 var arrangeWords = function(text) {
   return text
     .split(" ")
-    .sort((a, b) => {
-      if (a.length == b.length) return a - b;
-      else return a.length - b.length;
-    })
+    .sort((a, b) => a.length - b.length || a - b)
     .map(
       (w, idx) => (idx == 0 ? w.toUpperCase() : w.toLowerCase())[0] + w.slice(1)
     )
