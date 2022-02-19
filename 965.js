ddda1120b8881639b900dc2c24504f60c83b852b
LiewLi <liulimg@gmail.com>
2022-02-19 17:18:09 +0800

    sync

---
+++ 965.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isUnivalTree = function(root) {
+  if (!root) return true;
+  if (root.left) {
+    if (root.val !== root.left.val || !isUnivalTree(root.left)) return false;
+  }
+  if (root.right) {
+    if (root.val !== root.right.val || !isUnivalTree(root.right)) return false;
+  }
+
+  return true;
+};
