74bfc60694e441cca8649f475dbf9e876ec87dc6
liuli <liuli.nil@bytedance.com>
2022-03-22 21:24:40 +0800

    jest

---
+++ js/lc1624.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var maxLengthBetweenEqualCharacters = function(s) {
+  let ans  = -1;
+  const map = {};
+  for (let i = 0; i < s.length; ++i) {
+    if (map[s.charAt(i)] === undefined) {
+       map[s.charAt(i)] = i;
+    } else {
+      ans = Math.max(ans, i - map[s.charAt(i)] - 1);
+    }
+  }
+  return ans;
+};
+
+module.exports = maxLengthBetweenEqualCharacters;
+
