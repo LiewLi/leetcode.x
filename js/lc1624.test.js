4c2007ae092ac6dd62f0d6637b12726d2e56e16e
liuli <liuli.nil@bytedance.com>
2022-03-22 21:32:54 +0800

    lint

--- js/lc1624.test.js
+++ js/lc1624.test.js
@@ -1,5 +1,5 @@
-const maxLengthBetweenEqualCharacters = require("./lc1624")
+const maxLengthBetweenEqualCharacters = require("./lc1624");
 
-test('abca maxLength equal to 2', () => {
+test("abca maxLength equal to 2", () => {
   expect(maxLengthBetweenEqualCharacters("abca")).toBe(2);
 });
