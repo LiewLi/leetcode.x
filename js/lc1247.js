73770a880c52b3246c15420be0d3e2f300bb42bd
liuli <liuli.nil@bytedance.com>
2022-04-17 14:56:43 +0800

    sync

---
+++ js/lc1247.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
+ */
+var minimumSwap = function(s1, s2) {
+  if (s1.length != s2.length) return -1;
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < s1.length; ++i) {
+    if (s1.charCodeAt(i) == s2.charCodeAt(i)) continue;
+    else {
+      if (s1.charCodeAt(i) == "x".charCodeAt(0)) x += 1;
+      else y += 1;
+    }
+  }
+
+  if (x % 2 != y % 2) return -1;
+  if (x % 2 == 0) {
+    return x / 2 + y / 2;
+  } else {
+    return Math.floor(x / 2) + Math.floor(y / 2) + 2;
+  }
+};
+
+module.exports = minimumSwap;
